597e28b404c5795e53259bba3b0a40a8c6d09c2e
Sergei Elkin <sergeyelkin28@gmail.com>
2023-11-10 13:31:11 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppCard from './App/AppCard';
+import AppCard from './components/AppCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<AppCard />);
